d423f7ffc16a0c64a19e47562fbff8cb905bd7a6
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-25 16:26:59 +0300

    Refactor manifest.ts to remove theme color

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,6 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#000000",
     orientation: "portrait",
     lang: "tr",
     icons: [
