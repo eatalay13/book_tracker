cbe45744ea0a649d1a3a9809cf8d19cb07fcd2f5
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-25 13:20:14 +0300

    Refactor page title in page.tsx to include application name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeContainer from "@/containers/home";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sayfa sayısı Gir",
+  title: "Sayfa sayısı Gir | Risale-i Nur Okuma Takibi",
   description: "Risale-i Nur okuma takibi uygulaması",
 };
 
