ccde37818283403f18f1fa269cb4cb9343f62454
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-25 17:27:33 +0300

    Refactor BottomNavigation to use BottomNavigationItem component

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,38 +1,14 @@
 import { ChartArea, Home, ListOrdered, User } from "lucide-react";
-import Link from "next/link";
+import BottomNavigationItem from "./BottomNavigationItem";
 
 function BottomNavigation() {
   return (
-    <div className="fixed bottom-4 left-1/2 z-50 h-16 w-full max-w-lg -translate-x-1/2 rounded-full border border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-700">
+    <div className="fixed bottom-4 left-1/2 z-50 h-16 w-full max-w-lg -translate-x-1/2 rounded-3xl border border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-700">
       <div className="mx-auto flex h-full max-w-lg grid-cols-5 justify-around">
-        <Link
-          href="/"
-          className="group inline-flex flex-col items-center justify-center rounded-s-full px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
-        >
-          <Home />
-          <span className="sr-only">Anasayfa</span>
-        </Link>
-        <Link
-          href="/report"
-          className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
-        >
-          <ChartArea />
-          <span className="sr-only">Raporlar</span>
-        </Link>
-        <Link
-          href="/leaderboard"
-          className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
-        >
-          <ListOrdered />
-          <span className="sr-only">Okuma Sıralaması</span>
-        </Link>
-        <Link
-          href="/profile"
-          className="group inline-flex flex-col items-center justify-center rounded-e-full px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
-        >
-          <User />
-          <span className="sr-only">Profile</span>
-        </Link>
+        <BottomNavigationItem href="/" icon={<Home />} />
+        <BottomNavigationItem href="/report" icon={<ChartArea />} />
+        <BottomNavigationItem href="/leaderboard" icon={<ListOrdered />} />
+        <BottomNavigationItem href="/profile" icon={<User />} />
       </div>
     </div>
   );
---
+++ src/components/BottomNavigationItem.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface BottomNavigationItemProps {
+  icon: React.ReactNode;
+  href: string;
+}
+
+function BottomNavigationItem(props: BottomNavigationItemProps) {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={props.href}
+      className="group inline-flex flex-col items-center justify-center transition duration-200"
+    >
+      <div
+        className={clsx(
+          "rounded-full p-2",
+          pathname === props.href
+            ? "bg-red-500 text-white dark:bg-red-700"
+            : "bg-transparent",
+        )}
+      >
+        {props.icon}
+      </div>
+    </Link>
+  );
+}
+
+export default BottomNavigationItem;
