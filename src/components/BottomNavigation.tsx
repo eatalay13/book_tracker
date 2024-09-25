ceea113b9751799978bc21f3a9ff56bfa5aa3698
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-25 21:49:47 +0300

    Refactor styles for BottomNavigation and Card components

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -4,7 +4,7 @@ import BottomNavigationItem from "./BottomNavigationItem";
 function BottomNavigation() {
   return (
     <div className="fixed bottom-4 z-50 w-full">
-      <div className="w-full px-4">
+      <div className="w-full px-3">
         <div className="h-16 rounded-2xl border border-gray-200 bg-cardLight dark:border-zinc-800 dark:bg-cardDark">
           <div className="mx-auto flex h-full max-w-lg grid-cols-5 justify-around">
             <BottomNavigationItem href="/" icon={<Home />} />
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 function Card({ children }: CardProps) {
   return (
-    <div className="rounded-lg bg-cardLight p-6 shadow-xl dark:bg-cardDark">
+    <div className="rounded-lg border border-gray-200 bg-cardLight p-6 shadow-xl dark:border-zinc-800 dark:bg-cardDark">
       {children}
     </div>
   );
