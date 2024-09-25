8c023a157f829a2ae691178bf86222fcdae08be7
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-25 14:40:42 +0300

    Refactor BottomNavigation component to update navigation icons and add links

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,4 +1,4 @@
-import { ChartArea, Home, Settings, User } from "lucide-react";
+import { ChartArea, Home, ListOrdered, User } from "lucide-react";
 import Link from "next/link";
 
 function BottomNavigation() {
@@ -19,46 +19,20 @@ function BottomNavigation() {
           <ChartArea />
           <span className="sr-only">Raporlar</span>
         </Link>
-        <div
-          id="tooltip-new"
-          role="tooltip"
-          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
-        >
-          Yeni Sayfa Sayısı Ekle
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-        <button
-          data-tooltip-target="tooltip-settings"
-          type="button"
+        <Link
+          href="/leaderboard"
           className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
         >
-          <Settings />
-          <span className="sr-only">Settings</span>
-        </button>
-        <div
-          id="tooltip-settings"
-          role="tooltip"
-          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
-        >
-          Ayarlar
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-        <button
-          data-tooltip-target="tooltip-profile"
-          type="button"
+          <ListOrdered />
+          <span className="sr-only">Okuma Sıralaması</span>
+        </Link>
+        <Link
+          href="/profile"
           className="group inline-flex flex-col items-center justify-center rounded-e-full px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
         >
           <User />
           <span className="sr-only">Profile</span>
-        </button>
-        <div
-          id="tooltip-profile"
-          role="tooltip"
-          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
-        >
-          Profil
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
+        </Link>
       </div>
     </div>
   );
