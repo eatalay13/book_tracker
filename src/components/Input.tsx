af2f2b5afcfa697d92361a63a2a2f2b409dd4938
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-25 12:16:15 +0300

    Refactor Input component to disable autocomplete

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,7 +15,7 @@ const Input = (props: InputProps) => (
     name={props.name}
     placeholder={props.placeholder}
     onChange={props.onChange}
-    autoComplete={props.autoComplete || "on"}
+    autoComplete={props.autoComplete || "off"}
     className={`px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${props.className}`}
   />
 );
