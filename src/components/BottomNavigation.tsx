6254ff97b59d19daa4e5408116690027d2233942
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-25 14:37:34 +0300

    Refactor prettier configuration to include tailwindcss plugin and update formatting options

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,55 +1,28 @@
-import { ChartArea, Home, Plus, Settings, User } from "lucide-react";
+import { ChartArea, Home, Settings, User } from "lucide-react";
+import Link from "next/link";
 
 function BottomNavigation() {
   return (
-    <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
-      <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
-        <button
-          data-tooltip-target="tooltip-home"
-          type="button"
-          className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+    <div className="fixed bottom-4 left-1/2 z-50 h-16 w-full max-w-lg -translate-x-1/2 rounded-full border border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-700">
+      <div className="mx-auto flex h-full max-w-lg grid-cols-5 justify-around">
+        <Link
+          href="/"
+          className="group inline-flex flex-col items-center justify-center rounded-s-full px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
         >
           <Home />
-          <span className="sr-only">Home</span>
-        </button>
-        <div
-          id="tooltip-home"
-          role="tooltip"
-          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-        >
-          Anasayfa
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-        <button
-          data-tooltip-target="tooltip-wallet"
-          type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          <span className="sr-only">Anasayfa</span>
+        </Link>
+        <Link
+          href="/report"
+          className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
         >
           <ChartArea />
-          <span className="sr-only">Wallet</span>
-        </button>
-        <div
-          id="tooltip-wallet"
-          role="tooltip"
-          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-        >
-          Raporlar
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-        <div className="flex items-center justify-center">
-          <button
-            data-tooltip-target="tooltip-new"
-            type="button"
-            className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
-          >
-            <Plus className="text-white" />
-            <span className="sr-only">Yeni</span>
-          </button>
-        </div>
+          <span className="sr-only">Raporlar</span>
+        </Link>
         <div
           id="tooltip-new"
           role="tooltip"
-          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
         >
           Yeni Sayfa Sayısı Ekle
           <div className="tooltip-arrow" data-popper-arrow></div>
@@ -57,7 +30,7 @@ function BottomNavigation() {
         <button
           data-tooltip-target="tooltip-settings"
           type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
         >
           <Settings />
           <span className="sr-only">Settings</span>
@@ -65,7 +38,7 @@ function BottomNavigation() {
         <div
           id="tooltip-settings"
           role="tooltip"
-          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
         >
           Ayarlar
           <div className="tooltip-arrow" data-popper-arrow></div>
@@ -73,7 +46,7 @@ function BottomNavigation() {
         <button
           data-tooltip-target="tooltip-profile"
           type="button"
-          className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          className="group inline-flex flex-col items-center justify-center rounded-e-full px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
         >
           <User />
           <span className="sr-only">Profile</span>
@@ -81,7 +54,7 @@ function BottomNavigation() {
         <div
           id="tooltip-profile"
           role="tooltip"
-          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
         >
           Profil
           <div className="tooltip-arrow" data-popper-arrow></div>
