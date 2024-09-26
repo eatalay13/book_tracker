2e145b2d1345bbdd180554d9627692747862fafb
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-26 14:40:56 +0300

    Refactor middleware.ts to add authentication check for specific routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware } from "@/auth";
+import { MiddlewareConfig } from "next/server";
+import { auth } from "./auth";
+
+export default auth((req) => {
+  if (!req.auth) {
+    return Response.redirect(new URL("/api/auth/signin", req.url));
+  }
+});
+
+export const config: MiddlewareConfig = {
+  matcher: ["/", "/report", "/profile", "/leaderboard"],
+};
