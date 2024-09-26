e38046d9b8c2170d3b400f99b7d820c23a1c37fe
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-26 19:44:45 +0300

    Refactor home page to add motivational messages and improve readability

--- src/containers/home/HasTodayData.tsx
+++ src/containers/home/HasTodayData.tsx
@@ -1,12 +1,46 @@
+import { Award, Book } from "lucide-react";
+
 interface HasTodayDataProps {
   pageCount: number;
 }
 
 function HasTodayData({ pageCount }: HasTodayDataProps) {
+  const motivationalMessages = [
+    "Harika iş! Okumaya devam et ve zihnini geliştir.",
+    "Bir kitap, bir dünya demektir. Bugün yeni bir dünya keşfettin!",
+    "Okumak, ruhun gıdasıdır. Bugün ruhunu besledin!",
+    "Her sayfa yeni bir macera. Maceraya devam et!",
+  ];
+
+  const randomMessage =
+    motivationalMessages[
+      Math.floor(Math.random() * motivationalMessages.length)
+    ];
+
   return (
-    <div>
-      Tebrikler bugün veri girişi yapmışsınız. Bugün okuduğunuz sayfa sayısı:
-      {pageCount}
+    <div className="mx-auto max-w-sm overflow-hidden rounded-xl bg-gradient-to-br from-purple-500 to-red-500 shadow-lg dark:from-purple-800 dark:to-red-800">
+      <div className="space-y-4 p-6">
+        <div className="flex items-center justify-center">
+          <Book className="mr-2 text-white" size={28} />
+          <h4 className="text-lg font-semibold text-white">
+            Günlük Okuma Başarısı!
+          </h4>
+        </div>
+        <div className="text-center text-white">
+          <p className="text-opacity-90">{randomMessage}</p>
+        </div>
+        <div className="mt-4 flex items-center justify-center space-x-2">
+          <Award className="text-yellow-300" size={24} />
+          <span className="font-semibold text-white">
+            {pageCount} sayfa okudunuz.
+          </span>
+        </div>
+      </div>
+      <div className="bg-white bg-opacity-20 p-4">
+        <p className="text-center font-medium text-white">
+          Yarın da okumayı unutma! 📚
+        </p>
+      </div>
     </div>
   );
 }
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -10,17 +10,19 @@ interface HomeContainerProps {
 
 function HomeContainer(props: HomeContainerProps) {
   return (
-    <Card>
-      <h1 className="mb-6 flex items-center justify-center text-center text-2xl font-bold">
-        <BookOpen className="mr-2 inline-block" />
-        Risale Okuma Takibi
-      </h1>
+    <>
       {props.todayData ? (
         <HasTodayData pageCount={props.todayData.count} />
       ) : (
-        <CountSelect />
+        <Card>
+          <h1 className="mb-6 flex items-center justify-center text-center text-2xl font-bold">
+            <BookOpen className="mr-2 inline-block" />
+            Risale Okuma Takibi
+          </h1>
+          <CountSelect />
+        </Card>
       )}
-    </Card>
+    </>
   );
 }
 
