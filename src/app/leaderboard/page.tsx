2e3803c47e5ddc85db8c91bbaf2e3a99a46d7f2a
Emrah Atalay <emrahatalay92@gmail.com>
2024-09-26 17:33:52 +0300

    Refactor leaderboard page to display user rankings and page counts

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,25 +1,29 @@
+import Card from "@/components/Card";
 import { prisma } from "@/lib/prisma";
 
 async function getLeaderboard() {
-  const res = await prisma.user.findMany({
-    select: {
-      username: true,
-      PageCount: {
-        select: {
-          count: true,
-        },
-      },
+  const leaderboard = await prisma.pageCount.groupBy({
+    by: ["userId"],
+    _sum: {
+      count: true,
     },
     orderBy: {
-      PageCount: {
-        _count: "desc",
+      _sum: {
+        count: "desc",
       },
     },
+    take: 5,
   });
 
-  return res.map((user) => ({
-    username: user.username,
-    PageCount: user.PageCount,
+  const userIds = leaderboard.map((entry) => entry.userId);
+  const users = await prisma.user.findMany({
+    where: { id: { in: userIds } },
+    select: { id: true, username: true },
+  });
+
+  return leaderboard.map((entry) => ({
+    ...entry,
+    user: users.find((user) => user.id === entry.userId),
   }));
 }
 
@@ -27,31 +31,45 @@ async function Leaderboard() {
   const leaderboard = await getLeaderboard();
 
   return (
-    <div>
+    <Card>
       <h1>Liderlik Tablosu</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Kullanıcı Adı</th>
-            <th>Sayfa Sayısı</th>
-          </tr>
-        </thead>
-        <tbody>
-          {leaderboard.map((user, index) => (
-            <tr key={user.username}>
-              <td>
-                {index + 1}. {user.username}
-              </td>
-              <td>
-                {user.PageCount.map((count) => {
-                  return count.count;
-                })}
-              </td>
+
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+          <thead className="text-xs capitalize text-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Sıra
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Kullanıcı Adı
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Sayfa Sayısı
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {leaderboard.map((entry, index) => (
+              <tr key={entry.userId} className="border-b dark:border-gray-700">
+                <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+                  {index + 1}
+                </td>
+                <th
+                  scope="row"
+                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+                >
+                  {entry.user?.username}
+                </th>
+                <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+                  {entry._sum.count} Sayfa
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Card>
   );
 }
 
